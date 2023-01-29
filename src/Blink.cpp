c1106227c8b04d276e93b1689753a9a97c73b9aa
Tim Lunn <tim@feathertop.org>
2023-01-29 23:24:06 +1100

    update for blog post

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -5,27 +5,38 @@
  */
 
 #include <Arduino.h>
-
+int led_state;
 // Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 2
+#define LED_BUILTIN 2
 
 void setup()
 {
-  Serial.begin(115200);
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // initialise the serial monitor port
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Welcome to PlatformIO");
 }
 
 void loop()
 {
+
   // turn the LED on (HIGH is the voltage level)
-  Serial.println("High");
   digitalWrite(LED_BUILTIN, HIGH);
+  led_state=1;
+  Serial.println(LED_BUILTIN);
+  Serial.println("LED On");
+
   // wait for a second
   delay(1000);
+
   // turn the LED off by making the voltage LOW
-  Serial.println("Low");
   digitalWrite(LED_BUILTIN, LOW);
+  led_state=0;
+  Serial.println("LED Off");
+
    // wait for a second
   delay(1000);
 }
